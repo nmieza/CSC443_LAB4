2dc1be692f43224851ab0e6a1689350eef3e1093
nmieza <nurmieza14@gmail.com>
2022-04-28 10:40:16 +0800

    commit

--- handle_form.php
+++ handle_form.php
@@ -8,13 +8,10 @@
 </head>
 <body>
 <?php #Script handle form 
-    echo "<p> Thank you, <b>". 
-    $_POST["name"]
-    "</b> for the following comments: <br/>".
-    $_POST["comments"]
-    "<p>We will reply to you at <i>".
-    $_POST["email"]
-    "</i></p>";
-    ?>
+echo "<p> Thank you, <b>". 
+$_POST["name"]"</b> for the following comments: <br/>".
+$_POST["comments"]"<p>We will reply to you at <i>".
+$_POST["email"]"</i></p>"
+?>
 </body>
 </html>
