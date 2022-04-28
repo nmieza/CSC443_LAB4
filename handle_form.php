98a7539683761e7b9c21efccaeff971bcc675578
nmieza <nurmieza14@gmail.com>
2022-04-28 10:38:51 +0800

    commit

--- handle_form.php
+++ handle_form.php
@@ -4,13 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Feedback</title>
+    <title>Document</title>
 </head>
 <body>
-    
-    <?php #Script handle form 
-
-
+<?php #Script handle form 
     echo "<p> Thank you, <b>". 
     $_POST["name"]
     "</b> for the following comments: <br/>".
@@ -18,8 +15,6 @@
     "<p>We will reply to you at <i>".
     $_POST["email"]
     "</i></p>";
-    
     ?>
-
 </body>
 </html>
