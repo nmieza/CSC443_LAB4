22214f24286d7d944d74300f1aa8613312415603
nmieza <nurmieza14@gmail.com>
2022-05-09 23:03:37 +0800

    commit

--- handle_form.php
+++ handle_form.php
@@ -8,11 +8,11 @@
 </head>
 <body>
 <?php #Script handle form 
-if(strlen($_POST["name"]) >0) {
-    $name = $_POST["name"];
+if(isset($_POST['name']) >0) {
+    $name = $_POST['name'];
 }
 else{
-    $_POST["name"]= null;
+    $name = null;
 
     echo '<p><b> You Forgot to Enter Your Name! </b></p>'; 
 }
