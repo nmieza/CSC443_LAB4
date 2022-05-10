8ad52dc4010f5fd9bf263c9d4dd90d44b93a6e1a
nmieza <nurmieza14@gmail.com>
2022-05-10 20:06:07 +0800

    commit

--- handle_form.php
+++ handle_form.php
@@ -7,30 +7,28 @@
     <title>Document</title>
 </head>
 <body>
-<?php #Script handle form 
-if(isset($_POST["name"]) >0) {
-    $name = $_POST["name"];
+<?php #Script handle form
+
+if (strlen($_POST["name"]) >0){
+    $_POST["name"] = $_POST["name"];
 }
 else{
-    $name = null;
+    $_POST["name"]=null;
 
-    echo '<p><b> You Forgot to Enter Your Name! </b></p>'; 
+    echo '<p><b> You Forgot to Enter Your Name! </b></p>';
 }
-
-if(isset($_POST["comments"]) >0) {
-    $comments = $_POST["comments"];
+if (strlen($_POST["comments"]) >0){
+    $_POST["comments"] = $_POST["comments"];
 }
-else {
-    $comments= null;
+else{
+    $_POST["comments"]=null;
 
     echo '<p><b> You Forgot to Enter Your Comments! </b></p>';
 }
-if(isset($_POST["email"]) >0){
-    $email= null;
-
-    echo '<p><b> You Forgot to Enter Your Emails! </b></p>';
+if(!(strlen($_POST["email"]) >0)){
+$_POST["email"] = null;
+echo '<p><b> You Forgot to Enter Your Emails! </b></p>';
 }
-
 if(isset($_POST["gender"])){
     if($_POST["gender"] == 'M'){
         $message = '<b><p>Good Day, Sir </b></p>';
@@ -38,17 +36,19 @@ if(isset($_POST["gender"])){
     if($_POST["gender"] == 'F'){
         $message = '<b><p>Good Day, Madam </b></p>';
     }
-}else{
-    $_POST["gender"] = null;
-    echo '<p><b> You Forgot to Choose Your Gender! </b></p>';
 }
+else{
+    $_POST["gender"]=null;
 
+    echo '<p><b> You Forgot to Enter Your Gender! </b></p>';
+}
 if ($_POST["name"] && $_POST["comments"] && $_POST["email"] && $_POST["gender"]) {
-    echo "Thank you," .$_POST["name"]. " for the following comments: <br/> <tt>" .
-    $_POST["comments"]. " </tt> <p> We will reply to you at <i>" . $_POST["email"].
+    echo "Thank you," .$_POST["name"]. " for the following comments: <br /> <tt>".
+    $_POST["comments"]. " </tt> <p> We will reply to you at <i>" . $_POST["email"]. 
     "</i></p>";
     echo $message;
 }
 ?>
+
 </body>
 </html>
