9b4a74bf535369f92a4d531850bd3397e22cd609
nmieza <nurmieza14@gmail.com>
2022-05-10 20:16:27 +0800

    commit

--- handle_calculator.php
+++ handle_calculator.php
@@ -8,21 +8,22 @@
 </head>
 <body>
     <?php #script handle calculator
-     if (isset($source)){
-        if($source == 'calculator.html'){
+    
+    if(isset($source)){
+        if)$source == 'calculator.html'){
             if((is_numeric($quantity))){
                 $total = ($quantity * $price) * ($taxrate +1);
                 $total = number_format ($total,2);
                 echo "You are purchasing <b> ". $_POST["quantity"] . 
                 "</b> widgets at a cost  of <b> ". $_POST["price"] . 
-                "</b> each. With tax the total come to <b>". $total . "</b>.\n";
-            }else {
+                "</b> each. With tax the total come to <b>". $total . "</b>.\n"; 
+            }else{
                 echo "<p><b>Please enter a valid quantity to purchase!</b></p>";
             }
-        }else {
+        }else{
             echo "<p><b>You have accessed this page inappropriately!</b></p>";
         }
-    }else {
+    }else{
         echo "<p><b>You have accessed this  page inappropriately!</b></p>";
     }
     ?>
